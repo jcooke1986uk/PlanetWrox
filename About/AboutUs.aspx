0412d7f8f7f2101162a782b3bf8c20df053e7c75
jcooke1986uk <jcooke.web@gmail.com>
2017-01-03 20:38:55 +0000

    Chapter 8 User controls
     Finishing Chapter 8.  User controls and view state.

--- About/AboutUs.aspx
+++ About/AboutUs.aspx
@@ -4,5 +4,9 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
 <p>Hello</p>
+<p>Hello</p>
+<p>Hello</p>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    <Wrox:Banner ID="Banner2" runat="server" DisplayDirection="Horizontal" />
 </asp:Content>
 
--- Controls/Banner.ascx.cs
+++ Controls/Banner.ascx.cs
@@ -9,6 +9,58 @@ public partial class Controls_Banner : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        switch (DisplayDirection)
+        {
+            case Direction.Horizontal:
+                HorizontalPanel.Visible = true;
+                VerticalPanel.Visible = false;
+                HorizontalLink.HRef = NavigateUrl;
+                break;
+            case Direction.Vertical:
+                VerticalPanel.Visible = true;
+                HorizontalPanel.Visible = false;
+                VerticalLink.HRef = NavigateUrl;
+                break;
+        }
+    }
+    
+    public Direction DisplayDirection
+    {
+        get
+        {
+            object _displayDirection = ViewState["DisplayDirection"];
+            if (_displayDirection != null)
+            {
+                return (Direction)_displayDirection;
+            }
+            else
+            {
+                return Direction.Horizontal; //Return to a default value
+            }
+        }
+        set
+        {
+            ViewState["DisplayDirection"] = value;
+        }
+    }
 
+    public string NavigateUrl 
+    {
+        get 
+        {
+            object _navigateUrl = ViewState["NavigateUrl"];
+            if (_navigateUrl != null)
+            {
+                return (string)_navigateUrl;
+            }
+            else
+            {
+                return "http://p2p.wrox.com"; //Return to a default value
+            }
+        }
+        set
+        {
+            ViewState["NavigateUrl"] = value;
+        }
     }
 }
